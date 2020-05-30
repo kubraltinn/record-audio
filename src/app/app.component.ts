fd743379c1dae1a21b75943e134b0b338a15334f
Kübra Altın <kubra.altin@englishcentral.com>
2020-05-30 17:06:01 +0300

    recording audio and showing in volume meter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled3';
+  static BUFFER_SIZE = 2048;
+
+  title = 'Audio Recorder';
+  private audioSource: MediaStreamAudioSourceNode;
+  private audioAnalyser?: AnalyserNode;
+
+  static average(arr: Uint8Array): number {
+    return arr.reduce((prev, current) => prev + current, 0 ) / arr.length;
+  }
+
+  recordAudio() {
+    navigator.mediaDevices.getUserMedia({audio: true})
+      .then( (stream) => {
+        const audioContext = this.createAudioContext(stream);
+        const audioNode = audioContext.createScriptProcessor(AppComponent.BUFFER_SIZE, 1, 1);
+        audioNode.onaudioprocess = () => {
+          const audioByteArray = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+          this.audioAnalyser.getByteFrequencyData(audioByteArray);
+          const bar = document.getElementById('bar');
+          bar.style.width = AppComponent.average(audioByteArray) * 10 + 'px';
+        };
+        audioNode.connect(audioContext.destination);
+      })
+      .catch((error) => console.log('error', error));
+  }
+
+  stopRecording(): void {
+    this.audioSource.disconnect();
+  }
+
+  createAudioContext(stream: MediaStream): AudioContext | undefined {
+    const audioContext = new AudioContext();
+    this.audioSource = audioContext.createMediaStreamSource(stream);
+    this.audioAnalyser = audioContext.createAnalyser();
+    this.audioSource.connect(this.audioAnalyser);
+    return audioContext;
+  }
 }
